20398f79594aff51b5e386ef8e94c9c7a483ccb0
Batunde Abioro <batunde.abioro@gmail.com>
2024-07-08 11:15:21 +0100

    reworked

--- assignmentoncapitalletter.py
+++ assignmentoncapitalletter.py
@@ -1,14 +1,9 @@
-def is_proper_name(name):
-    # Check if the first character is uppercase and the rest are lowercase
-    return name[0].isupper() and name[1:].islower()
+name = input("Enter a Name " )
+if name is capitalised_name:
+    print(f" The name {name} is properly capitalised " )
+else:
+    print(f" The name {name} is not properly capitalised ")
 
-def main():    
-    name = input("Enter a name: ")
-    
-    # Check if the name is properly capitalized
-    if is_proper_name(name):
-        print(f"The name '{name}' is properly capitalized.")
-    else:
-        print(f"The name '{name}' is not properly capitalized.")
-if __name__ == "__main__":
-    main()
+
+
+
